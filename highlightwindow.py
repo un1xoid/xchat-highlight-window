61e48d939e7a158765c681205e6169dd8f48a698
Viktor Ashirov <vashirov@users.noreply.github.com>
2016-05-03 23:17:31 +0200

    Add highlights for private messages

--- highlightwindow.py
+++ highlightwindow.py
@@ -1,10 +1,10 @@
+import xchat
+
 __module_name__ = "HighlightWindow"
-__module_version__ = "1.0"
+__module_version__ = "1.1"
 __module_description__ = "A simple plugin to aggregate all highlights into a single, easy to read window."
 
-print "\0034", __module_name__, __module_version__, "has been loaded\003"
-
-import xchat
+xchat.prnt('\0034%(name)s, version %(version)s has been loaded\003' % {'name': __module_name__,  'version': __module_version__})
 
 
 def highlight(word, word_eol, userdata):
@@ -17,8 +17,24 @@ def highlight(word, word_eol, userdata):
     server = context.get_info('server')
 
     query = xchat.find_context(server, '@highlight')
-    query.prnt('[' + channel + '] <' + nick + '> ' + message[:-2])
+    query.prnt('[' + channel + '] <' + nick + '> ' + message)
     query.command("GUI COLOR 3")
+
+
+def highlight_private(word, word_eol, userdata, private=False):
+    context = xchat.get_context()
+    context.command('query -nofocus @highlight')
+
+    nick = word[0]
+    message = word_eol[1]
+    server = context.get_info('server')
+
+    query = xchat.find_context(server, '@highlight')
+    query.prnt('[private msg] <' + nick + '> ' + message)
+    query.command("GUI COLOR 3")
+
 
 xchat.hook_print('Channel Msg Hilight', highlight)
 xchat.hook_print('Channel Action Hilight', highlight)
+#xchat.hook_print('Private Message', highlight)
+xchat.hook_print('Private Message to Dialog', highlight_private)
